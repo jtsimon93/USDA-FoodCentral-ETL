8899801c20043541fa282c82bfcf9d59e6b6374b
Justin Simon <jtsimon@jtsimon.dev>
2025-05-19 20:59:57 -0400

    Update data type to support the full CSV food file

--- include/models/usda/Food.h
+++ include/models/usda/Food.h
@@ -11,7 +11,7 @@ typedef struct {
   int fdc_id; // Primary key (internal to this table)
   FoodDataType data_type;
   std::string description;
-  std::optional<int> food_category_id;
+  std::optional<std::string> food_category_id;
   std::chrono::year_month_day publication_date;
 } Food;
 } // namespace USDA
